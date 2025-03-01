8d3c485e1a5d148de27432f09c90fc6ca052860b
LaxmiKoushik N <koushik@elberttech.com>
2025-03-01 21:02:47 +0530

    new changes

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -12,13 +12,13 @@ function CarouselPage() {
     <div className="container mt-5 ">
       <Carousel>
         {/* Slide 1 */}
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <img className="d-block w-full carousel-img" src={image1} alt="Big Data" />
           <Carousel.Caption>
             <h3>Big Data Solutions</h3>
             <p>Unlock insights with our cutting-edge data analytics services.</p>
           </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
 
         {/* Slide 2 */}
         <Carousel.Item>
@@ -29,12 +29,20 @@ function CarouselPage() {
           </Carousel.Caption>
         </Carousel.Item>
 
+
         {/* Slide 3 */}
         <Carousel.Item>
           <img className="d-block w-100 carousel-img" src={image3} alt="Cloud Services" />
           <Carousel.Caption>
             <h3>Cloud Services</h3>
             <p>Reliable and scalable cloud solutions for your business.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 carousel-img" src={image2} alt="Business Intelligence" />
+          <Carousel.Caption>
+            <h3>Business Intelligence</h3>
+            <p>Turn data into actionable insights with our BI tools.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
